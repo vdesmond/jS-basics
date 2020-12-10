dd08e60a9eb52ef8dc4f6b4f380f6b8cfb9b4f23
Vignesh-Desmond <vignesh.desmond@gmail.com>
2020-12-10 19:28:42 +0530

    capture when enter

--- gif-search-engine/GiphySearch/javascript/main.js
+++ gif-search-engine/GiphySearch/javascript/main.js
@@ -5,19 +5,29 @@ function getInput() {
   return inputText;
 }
 
-function pushImages(input) {
+//Push GIFs to container
+function pushGifs(input) {
   var container = document.querySelector(".js-container");
   container.innerHTML = input;
 }
 
+// Capture text in Search box
+// Capture when button pressed
 document.querySelector("button").addEventListener("click", function () {
   var inputText = document.querySelector("input").value;
   console.log(inputText);
-  pushImages(inputText);
+  pushGifs(inputText);
 });
 
-document.querySelector(".js-userinput").addEventListener("keyup", function () {
-  var inputText = document.querySelector("input").value;
-  console.log(inputText);
-  pushImages(inputText);
-});
+//Capture when press "Enter"
+document
+  .querySelector(".js-userinput")
+  .addEventListener("keyup", function (event) {
+    var inputText = document.querySelector("input").value;
+    /*  console.log(event);
+    console.log(inputText); */
+    // Enter key
+    if (event.which == 13) {
+      pushGifs(inputText);
+    }
+  });
